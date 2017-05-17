9c040fbb7c6fbe1244eb55f5260e2b3d25d3d3f5
Jotta <Jotta@JOTTA-PC>
2017-05-17 19:57:08 +0100

    Add project files.

---
+++ SnakeDLL/SnakeDLL.c
@@ -0,0 +1,221 @@
+
+#include <windows.h>
+#include "SnakeDLL.h"
+
+//Definição da variável global
+HANDLE hMemoria;
+HANDLE hSemMemoria;
+HANDLE hEventoMemoria;
+HANDLE hFicheiro;
+MemGeral *vistaPartilhaGeral;
+
+void criaCobra(TCHAR username[SIZE_USERNAME], int vaga);
+
+int preparaMemoriaPartilhada(void) {
+
+	hFicheiro = CreateFile(FILE_MAP_NAME, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	hMemoria = CreateFileMapping(hFicheiro, NULL, PAGE_READWRITE, 0, SIZE_MEM_GERAL, NOME_MEM_GERAL);
+
+	vistaPartilhaGeral = (MemGeral*)MapViewOfFile(hMemoria, FILE_MAP_ALL_ACCESS, 0, 0, SIZE_MEM_GERAL);
+
+	hEventoMemoria = CreateEvent(NULL, TRUE, FALSE, EVNT_MEM_GERAL);
+	hSemMemoria = CreateSemaphore(NULL, MAXCLIENTES, MAXCLIENTES, SEM_MEM_GERAL);
+
+	srand((int)time(NULL));
+
+	if (hMemoria == NULL || hEventoMemoria == NULL || hSemMemoria == NULL) {
+		_tprintf(TEXT("[Erro] Criação de objectos do Windows(%d)\n"), GetLastError());
+		return -1;
+	}
+}
+
+void preparaMapaJogo(MemGeral param) {
+
+	for (int i = 0; i < MAXCLIENTES; i++) {
+		WaitForSingleObject(hSemMemoria, INFINITE);
+	}
+
+	for (int z = 0; z < param.config.C; z++) {
+		vistaPartilhaGeral->mapa[0][z] = TEXT('#');
+		vistaPartilhaGeral->mapa[param.config.L - 1][z] = TEXT('#');
+		for (int j = 1; j < param.config.L - 1; j++) {
+			if (z == 0 || z == param.config.C - 1) {
+				vistaPartilhaGeral->mapa[j][z] = TEXT('#');
+			}
+			else
+				vistaPartilhaGeral->mapa[j][z] = TEXT(' ');
+		}
+	}
+	SetEvent(hEventoMemoria);
+	ResetEvent(hEventoMemoria);
+	ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+}
+
+void inicializaMemoriaPartilhada(void) {
+	//Inicialização da Memoria Partilhada
+	vistaPartilhaGeral->estadoJogo = CRIACAOJOGO;
+}
+
+void esperaPorActualizacao(void) {
+	WaitForSingleObject(hEventoMemoria, INFINITE);
+}
+
+void leMemoriaPartilhada(MemGeral* param) {
+
+	WaitForSingleObject(hSemMemoria, INFINITE);
+
+	param->estadoJogo = vistaPartilhaGeral->estadoJogo;
+	param->mensagem.codigoMsg = vistaPartilhaGeral->mensagem.codigoMsg;
+	_tcscpy_s(param->mensagem.username, SIZE_USERNAME, vistaPartilhaGeral->mensagem.username);
+	param->config.C = vistaPartilhaGeral->config.C;
+	param->config.L = vistaPartilhaGeral->config.L;
+
+	ReleaseSemaphore(hSemMemoria, 1, NULL);
+}
+
+void fechaMemoriaPartilhada(void) {
+	CloseHandle(hMemoria);
+	CloseHandle(hSemMemoria);
+	CloseHandle(hEventoMemoria);
+	UnmapViewOfFile(vistaPartilhaGeral);
+}
+
+void getMapa(MemGeral *param) {
+	WaitForSingleObject(hSemMemoria, INFINITE);
+	for (int i = 0; i < vistaPartilhaGeral->config.L; i++) {
+		for (int j = 0; j < vistaPartilhaGeral->config.C; j++) {
+			param->mapa[i][j] = vistaPartilhaGeral->mapa[i][j];
+		}
+	}
+	ReleaseSemaphore(hSemMemoria, 1, NULL);
+}
+
+int Cria_Jogo(ConfigInicial param,TCHAR username1[SIZE_USERNAME]) {
+	for (int i = 0; i < MAXCLIENTES; i++) {
+		WaitForSingleObject(hSemMemoria, INFINITE);
+	}
+	if ((vistaPartilhaGeral->estadoJogo != CRIACAOJOGO)) {
+		ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+		return 0;
+	}
+	
+	vistaPartilhaGeral->estadoJogo = ASSOCIACAOJOGO;
+	vistaPartilhaGeral->config = param;
+	vistaPartilhaGeral->mensagem.codigoMsg = CRIARJOGO;
+	_tcscpy_s(vistaPartilhaGeral->mensagem.username, SIZE_USERNAME, username1);
+	_tcscpy_s(vistaPartilhaGeral->criador, SIZE_USERNAME, username1);
+	vistaPartilhaGeral->vagasJogadores = 0;
+
+	SetEvent(hEventoMemoria);
+	ResetEvent(hEventoMemoria);
+	ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+	return 1;
+}
+
+int IniciaJogo(TCHAR username[SIZE_USERNAME]) {
+	for (int i = 0; i < MAXCLIENTES; i++) {
+		WaitForSingleObject(hSemMemoria, INFINITE);
+	}
+
+	//Se não for o user que criou o jogo não pode dar inicio a este
+	if (!(_tcscmp(username, vistaPartilhaGeral->criador)==0)) {
+		ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+		return 0;
+	}
+	vistaPartilhaGeral->mensagem.codigoMsg = INICIARJOGO;
+	_tcscpy_s(vistaPartilhaGeral->mensagem.username, SIZE_USERNAME, username);
+
+	SetEvent(hEventoMemoria);
+	ResetEvent(hEventoMemoria);
+	ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+	return 1;
+}
+
+
+int AssociaJogo(int numJogadores, TCHAR username1[SIZE_USERNAME], TCHAR username2[SIZE_USERNAME], int *indice) {
+	for (int i = 0; i < MAXCLIENTES; i++) {
+		WaitForSingleObject(hSemMemoria, INFINITE);
+	}
+	//Se não existir jogo criado e as vagas existentes não forem suficientes para os jogadores que se pretendem juntar ao jogo
+	if ((vistaPartilhaGeral->estadoJogo != ASSOCIACAOJOGO) && ((vistaPartilhaGeral->config.N - vistaPartilhaGeral->vagasJogadores) < numJogadores)) {
+		ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+		return 0;
+	}
+	*indice = vistaPartilhaGeral->vagasJogadores;
+
+	if (numJogadores == 1) {
+		criaCobra(username1, vistaPartilhaGeral->vagasJogadores);
+		vistaPartilhaGeral->vagasJogadores++;
+	}
+	else {
+		criaCobra(username1, vistaPartilhaGeral->vagasJogadores);
+		vistaPartilhaGeral->vagasJogadores++;
+		criaCobra(username2, vistaPartilhaGeral->vagasJogadores);
+		vistaPartilhaGeral->vagasJogadores++;
+	}
+
+	SetEvent(hEventoMemoria);
+	ResetEvent(hEventoMemoria);
+	ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+	return 1;
+}
+
+void mudaDirecao(int direcao, int indice) {
+	for (int i = 0; i < MAXCLIENTES; i++) {
+		WaitForSingleObject(hSemMemoria, INFINITE);
+	}
+
+	//Hardcoded a posicao do array mas tem de ser mudado este aspecto
+	vistaPartilhaGeral->jogadores[indice].direcao = direcao;
+
+	ReleaseSemaphore(hSemMemoria, MAXCLIENTES, NULL);
+	return 1;
+}
+
+//Gera as posições da cobra no mapa verificando se há colisões com paredes e fazendo a respectiva alteração á cobra
+void criaCobra(TCHAR username[SIZE_USERNAME], int vaga) {
+	int posXGerada, posYGerada, dirGerada;
+	//Gera posições até encontrar uma vaga;
+	while (1) {
+		posXGerada = random_at_most((long)vistaPartilhaGeral->config.C);
+		posYGerada = random_at_most((long)vistaPartilhaGeral->config.L);
+		if (vistaPartilhaGeral->mapa[posYGerada][posXGerada] == ' ')
+			break;
+	}
+
+	//Na posição 0 do array de posições ficam as Linhas e na 1 ficam as Colunas
+	vistaPartilhaGeral->jogadores[vaga].posicoesCobra[0][0] = posYGerada;
+	vistaPartilhaGeral->jogadores[vaga].posicoesCobra[0][1] = posXGerada;
+	vistaPartilhaGeral->mapa[posYGerada][posXGerada] = vaga + '0';
+
+	dirGerada = random_at_most(3) + 1;
+	vistaPartilhaGeral->jogadores[vaga].direcao = dirGerada;
+
+	vistaPartilhaGeral->jogadores[vaga].porAparecer = vistaPartilhaGeral->config.T - 1;
+	vistaPartilhaGeral->jogadores[vaga].estadoJogador = VIVO;
+	vistaPartilhaGeral->jogadores[vaga].pontuacao = 0;
+	vistaPartilhaGeral->jogadores[vaga].tamanho = vistaPartilhaGeral->config.T;
+	_tcscpy_s(vistaPartilhaGeral->jogadores[vaga].username, SIZE_USERNAME, username);
+}
+
+// Assumes 0 <= max <= RAND_MAX
+// Returns in the closed interval [0, max]
+long random_at_most(long max) {
+	unsigned long
+		// max <= RAND_MAX < ULONG_MAX, so this is okay.
+		num_bins = (unsigned long)max + 1,
+		num_rand = (unsigned long)RAND_MAX + 1,
+		bin_size = num_rand / num_bins,
+		defect = num_rand % num_bins;
+
+	long x;
+	do {
+		x = rand();
+	}
+	// This is carefully written not to overflow
+	while (num_rand - defect <= (unsigned long)x);
+
+	// Truncated division is intentional
+	return x / bin_size;
+}
---
+++ SnakeDLL/SnakeDLL.h
@@ -0,0 +1,48 @@
+//SnakeDLL.h
+// O bloco ifdef seguinte é o modo standard de criar macros que tornam a exportação de
+// funções e variáveis mais simples. Todos os ficheiros neste projeto DLL são
+// compilados com o símbolo DLL_IMP_EXPORTS definido. Este símbolo não deve ser definido
+// em nenhum projeto que use a DLL. Desta forma, qualquer outro projeto que inclua este
+// este ficheiro irá ver as funções e variáveis DLL_IMP_API como sendo importadas de uma
+// DLL.
+
+#include <windows.h>
+#include <tchar.h>
+#include "TiposConstantes.h"
+
+//Definir uma constante para facilitar a leitura do protótipo da função
+//Este .h deve ser incluído no projeto que o vai usar (modo implícito)
+
+//Esta macro é definida pelo sistema caso estejamos na DLL (<DLL_IMP>_EXPORTS definida)
+//ou na app (<DLL_IMP>_EXPORTS não definida) onde DLL_IMP é o nome deste projeto
+//#ifdef DLL_EXPORTS
+
+//**********************************************************************************
+#ifdef DLL_EXPORTS   // VEIO DE -> PROP PROJ -> C++ -> LINHA COMANDO   -->  Está lá o nome do proj + o EXPORT, copiar para aqui
+//**********************************************************************************
+
+#define DLL_IMP_API __declspec(dllexport)   // Deve ser acrescentado antes de cada declaração de função ****************
+#else										// ou var global a usar fora da DLL, é para exportar		****************
+#define DLL_IMP_API __declspec(dllimport)   // Se estiver na DLL faz o de sima, se não estiver faz este
+#endif
+
+//Variável global da DLL
+extern DLL_IMP_API HANDLE hMemoria;
+extern DLL_IMP_API HANDLE hSemMemoria;
+extern DLL_IMP_API HANDLE hEventoMemoria;
+extern DLL_IMP_API HANDLE hFicheiro;
+extern DLL_IMP_API MemGeral *vistaPartilhaGeral;
+
+//Funções a serem exportadas/importadas
+DLL_IMP_API int preparaMemoriaPartilhada(void);
+DLL_IMP_API void preparaMapaJogo(MemGeral param);
+DLL_IMP_API void inicializaMemoriaPartilhada(void);
+DLL_IMP_API void esperaPorActualizacao(void);
+DLL_IMP_API void leMemoriaPartilhada(MemGeral* param);
+DLL_IMP_API void fechaMemoriaPartilhada(void);
+DLL_IMP_API int Cria_Jogo(ConfigInicial param, TCHAR username1[SIZE_USERNAME]);
+DLL_IMP_API void getMapa(MemGeral *param);
+DLL_IMP_API void mudaDirecao(int direcao, int indice);
+DLL_IMP_API int AssociaJogo(int numJogadores, TCHAR username1[SIZE_USERNAME], TCHAR username2[SIZE_USERNAME], int *indice);
+DLL_IMP_API int IniciaJogo(TCHAR username[SIZE_USERNAME]);
+
